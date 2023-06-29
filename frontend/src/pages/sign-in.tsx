4021a67edc33592ce2542dc10275ee2ae10112a7
labara1 <adlabaran@gmail.com>
2023-06-29 23:10:02 +0000

    login and signUP page

--- frontend/src/pages/sign-in.tsx
+++ frontend/src/pages/sign-in.tsx
@@ -1,5 +1,49 @@
 import React from 'react';
+import { TextField, Button } from '@mui/material';
+import Styles from '../styles/auth/sign-in.module.css';
+import Link from 'next/link';
 
-export default function SignIn() {
-  return <div>SignIN</div>;
-}
+const SignIn: React.FC = () => {
+  const [email, setEmail] = React.useState<string>('');
+  const [password, setPassword] = React.useState<string>('');
+  return (
+    <div>
+      <div className={Styles.SignInContainer}>
+        <div className={Styles.signIn}>
+          <h1 style={{ alignSelf: 'center' }}>Sign In</h1>
+          <form
+            style={{ display: 'flex', flexDirection: 'column', gap: '2rem' }}
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <TextField
+              type='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              label='Email'
+            />
+            <TextField
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              label='Password'
+            />
+
+            <div className={Styles.forgotLink}>
+              <Link href='#'> Forgot your password ?</Link>
+            </div>
+
+            <div className={Styles.btnSignIn}>
+              <button type='submit'>Sign In</button>
+
+              <Link href='/sign-up'> Don&apos;t have an account? Sign Up</Link>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
--- frontend/src/pages/sign-up.tsx
+++ frontend/src/pages/sign-up.tsx
@@ -1,5 +1,52 @@
 import React from 'react';
+import { TextField, Button } from '@mui/material';
+import Styles from '../styles/auth/sign-in.module.css';
+import Link from 'next/link';
 
-export default function SignUp() {
-  return <div>Sign Up</div>;
-}
+const SignUp: React.FC = () => {
+  const [email, setEmail] = React.useState<string>('');
+  const [password, setPassword] = React.useState<string>('');
+  const [passwordConfirm, setPasswordConfirm] = React.useState<string>('');
+  return (
+    <div>
+      <div className={Styles.SignInContainer}>
+        <div className={Styles.signIn}>
+          <h1 style={{ alignSelf: 'center' }}>Sign Up</h1>
+          <form
+            style={{ display: 'flex', flexDirection: 'column', gap: '2rem' }}
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <TextField
+              type='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              label='Email'
+            />
+            <TextField
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              label='Password'
+            />
+            <TextField
+              type='password'
+              value={passwordConfirm}
+              onChange={(e) => setPasswordConfirm(e.target.value)}
+              label='Confirm Password'
+            />
+
+            <div className={Styles.btnSignIn}>
+              <button type='submit'>Sign Up</button>
+
+              <Link href='/sign-in'> Already have an account? Sign In</Link>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
