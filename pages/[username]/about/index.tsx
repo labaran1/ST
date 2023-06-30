4a22dd7f01fb409d09f855858c1935555e528057
labara1 <adlabaran@gmail.com>
2023-06-30 12:21:31 +0000

    del

--- pages/[username]/about/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { useRouter } from 'next/router';
-
-export default function About() {
-  const router = useRouter();
-  const { username } = router.query;
-  return <div>{username} About</div>;
-}
--- pages/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <>Dash board contain feed</>;
-}
