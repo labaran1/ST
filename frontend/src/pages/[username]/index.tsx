f51e6d377dc4743555a1326f3fb629a1b7469a3d
labara1 <adlabaran@gmail.com>
2023-06-30 23:36:32 +0000

    m

--- frontend/src/pages/[username]/index.tsx
+++ frontend/src/pages/[username]/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import { fetcher } from '@/utils/apiaxios';
+import useSWR from 'swr';
+
 export default function Profile() {
   const router = useRouter();
   const { username } = router.query;
   return (
-    <>
+    <div>
       <h1>Profile {username}</h1>
-    </>
+    </div>
   );
 }
